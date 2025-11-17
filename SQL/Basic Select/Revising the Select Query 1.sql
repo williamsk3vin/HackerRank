5a5282f79bb0d01d4c5c58f34069429d5020907a
williamsk3vin <williamsk3vin@gmail.com>
2025-11-17 07:42:56 -0800

    Update Revising the Select Query 1.sql

--- SQL/Basic Select/Revising the Select Query 1.sql
+++ SQL/Basic Select/Revising the Select Query 1.sql
@@ -1,3 +1,8 @@
+/*
+Query all columns for all American cities in the CITY table with populations larger than 100000. 
+The CountryCode for America is USA. 
+*/
+
 SELECT *  
 From CITY 
 WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
