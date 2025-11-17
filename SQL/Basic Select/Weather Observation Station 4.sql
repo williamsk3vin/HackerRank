c628c99ca4dce1593a8a7e96b45efb85c75c1ecb
williamsk3vin <williamsk3vin@gmail.com>
2025-11-17 07:28:07 -0800

    Create Weather Observation Station 4.sql

---
+++ SQL/Basic Select/Weather Observation Station 4.sql
@@ -0,0 +1,7 @@
+/*
+Find the difference between the total number of CITY entries in the table 
+and the number of distinct CITY entries in the table. 
+*/
+
+SELECT (count(city) - count(DISTINCT(CITY)))
+FROM STATION;
