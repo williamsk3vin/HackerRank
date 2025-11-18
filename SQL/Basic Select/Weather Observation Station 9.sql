1fc566ebf664b382ac879826b130c73708619340
williamsk3vin <williamsk3vin@gmail.com>
2025-11-18 06:55:12 -0800

    Create Weather Observation Station 9.sql

---
+++ SQL/Basic Select/Weather Observation Station 9.sql
@@ -0,0 +1,7 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE substring(CITY,1,1) NOT IN ('A','E','I','O','U');
